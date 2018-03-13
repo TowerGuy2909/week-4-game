676bf43929fac531b4dc37f9828840e6b58b1f9c
Jerridd <jerridd_speidel@yahoo.com>
2018-03-12 19:54:32 -0500

    created cyrstalValue function to create all crystals numbers instead of one at a time.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,28 +5,33 @@ var losses = 0;
 var turns = 10;
 var totalScore = 0;
 var crystals = ['ruby','saphire','topaz','emrald'];
+var newScore = 0;
+var playerScore = 0;
 
-
-ruby = Math.floor(Math.random()*111)+20;
-
-saphire = Math.floor(Math.random()*111)+20;
-
-topaz = Math.floor(Math.random()*111)+20;
-
-emrald = Math.floor(Math.random()*111)+20;
-
-
-function rubyScore(){
-	totalScore + crystals[0];
+function crystalValue(){
+	for(var i = 0; i < crystals.length; i++){
+		crystals[i] = Math.floor(Math.random()*111)+20;
+		console.log(crystals[i]);
+	}
 }
+
+$("btn1").click(function() {
+	console.log("this is printing");
+	
+});
+console.log("testin addition " + (playerScore = newScore + ruby) );
+console.log("newScore equals " + newScore);
+console.log("totalScore equals " + totalScore);
+console.log("playerScore equals " + playerScore);
+
 function saphireScore(){
-	totalScore + saphire;
+	totalScore + crystals[1];
 }
 function topazScore(){
-	totalScore + topaz;
+	totalScore + crystals[2];
 }
 function emraldScore(){
-	totalScore + emrald;
+	totalScore + crystals[3];
 }
 
 function turnCounter(){
