3677e3f2086fd4322f3d7b202b490c255d66349e
Jerridd <jerridd_speidel@yahoo.com>
2018-03-15 11:20:17 -0500

    testing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,30 @@
+// var ruby;
+// var targetScore = 0;
+// crystals = [];
 
-var randomGoalScore = Math.floor(Math.random()*111)+20;
-var wins = 0;
-var losses = 0;
-var turns = 10;
-var totalScore = 0;
-var crystals = [0, 0, 0 , 0];
-var newScore = 0;
-var playerScore = 0;
+// $('#targetScore').text(targetScore);
 
 
-function creatingCrystals(){
-	for(var i = 0; i < crystals.length; i++){
-		crystals[i] = Math.floor(Math.random()*111)+20;
+// for (var i = 0; i < 4; i++) {
+// 	crystals[i] = Math.floor(Math.random()*111)+20;
+// 	console.log("the crystals are " + crystals[i]);
+// }
+
+
+
+$("#ruby").on("click", function(){
+
+	alert("test");
+
+});
 		
-	}
-}
 
-console.log("this should but the " + crystals[0]);
-
-
-
-$("#btn1").on("click", function() {
-	
-	alert(totalScore += 3);
-	
+$("#test").on("click", function(){
+	alert("please");
 });
 
-
-function saphireScore(){
-	totalScore + crystals[1];
-}
-function topazScore(){
-	totalScore + crystals[2];
-}
-function emraldScore(){
-	totalScore + crystals[3];
-}
-
-function turnCounter(){
-	if(totalScore < randomGoalScore){
-		turns = turns--;
-	}else if(totalScore > randomGoalScore){
-		document.write('You Lose');
-	}else {
-		document.write('You Win!');
-	}
-}
-function gemClick(index){
-	var v = crystals[index];
-	alert(v);
-}
-
-$().ready(function(){
-	$("#targetScore").text(randomGoalScore);
-	
-	// for (i = 0; i < crystals.length; i++);{
-	// 	crystals[i] = Math.floor(Math.random()*12)+1;
-	// }
+$(document).ready(function(){
+    $("button").click(function(){
+        $("p").hide();
+    });
 });
